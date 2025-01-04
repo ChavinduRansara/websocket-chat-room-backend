e40bde399580e179228cb5856775bed11a12aba7
ChavinduRansara <119533194+ChavinduRansara@users.noreply.github.com>
2025-01-04 14:31:20 +0530

    first commit

---
+++ src/main/java/com/chavindu/chat/chat/ChatMessage.java
@@ -0,0 +1,15 @@
+package com.chavindu.chat.chat;
+
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class ChatMessage {
+
+    private String content;
+    private String sender;
+    private MessageType type;
+}
---
+++ src/main/java/com/chavindu/chat/chat/MessageType.java
@@ -0,0 +1,8 @@
+package com.chavindu.chat.chat;
+
+public enum MessageType {
+
+    CHAT,
+    JOIN,
+    LEAVE
+}
